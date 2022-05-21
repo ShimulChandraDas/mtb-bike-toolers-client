8aa094cbef5329d5c381f0cd13533fbadfbb87df
ShimulChandraDas <msshimul99@gmail.com>
2022-05-21 23:47:46 +0600

    structure ready

---
+++ src/Shared/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2 className='text-red-800'>This Is Error</h2>
+        </div>
+    );
+};
+
+export default NotFound;
